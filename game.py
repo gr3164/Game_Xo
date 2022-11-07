7cdd21eb227c4eb39b3392c05bf9b7d908e26087
Grisha Pinigin <pin.pinigin@mail.ru>
2022-11-07 23:00:47 +0500

    add new win_combination

--- game.py
+++ game.py
@@ -2,8 +2,19 @@ import random
 
 box = [['\u00B7','\u00B7','\u00B7'], ['\u00B7','\u00B7','\u00B7'], ['\u00B7','\u00B7','\u00B7']]
 unique = [(x,y) for x in range(0,3) for y in range(0,3)]
+
 # bot = random.choice(unique)
 
+def check_win(box):
+    win_comb = (((0,0),(0,1),(0,2)), ((1,0),(1,1),(1,2)), ((2,0),(2,1),(2,2)),
+                ((0,0),(1,0),(2,0)), ((0,1),(1,1),(2,1)), ((0,2),(1,2),(2,2)),
+                ((0,0),(1,1),(2,2)), ((0,2),(1,1),(2,0)))
+    for i in win_comb:
+        if box[i[0][0]][i[0][1]] == box[i[1][0]][i[1][1]] == box[i[2][0]][i[2][1]] != '\u00B7':
+            return box[i[0][0]][i[0][1]]
+    return False
+    
+    
 def Print_box(box, key):
     if key == 'y':
         j = 1
@@ -27,40 +38,17 @@ def Compare_unique(p, unique):
         return p
     else: return p
 #=======================================================
-          
-def Win_g(box):
-    mylist = [ i[x][0] for x in range(0,3) for i in box]
-    mylist = [mylist[i:i + 3] for i in range(0, len(mylist), 3)]
-    for i in range(0,3):
-        for x in box:
-            if len(x) == x.count(x[i]) and not '\u00B7' in x or len(mylist[i]) == mylist[i].count(mylist[i][0]) and not '\u00B7' in mylist[i]:
-                return True
-            else:
-                return Win_x(box)
-        
 
-def Win_x(box):
-    mylist_x = [[],[]]
-    i = 0
-    j = -1
-    for x in box:
-        mylist_x[0].append(x[i])
-        mylist_x[1].append(x[j])
-        i += 1
-        j -= 1
-    for i in range(0,2):
-        if len(mylist_x[i]) == mylist_x[i].count(mylist_x[i][0]) and not '\u00B7' in mylist_x[i]:
-            return True
-        else:
-            False
 
-      
 
 print_key = input("Отоброжать напровляющие(Ряд/Столб) Y/N: ").lower()
 # choice = str(input("Будете ходить первым? y/n: ").lower())
 
 while True:
-    Print_box(box, print_key) 
+    Print_box(box, print_key)
+    if check_win(box) != False:
+        print("Победа " + check_win(box))
+        break 
 
     if 0 < len(unique):
         #user Ввод пользователя + проверка на уникальность + удаление элемента из уникального списка
@@ -68,10 +56,9 @@ while True:
         user = Compare_unique(user, unique)
         unique.remove(user)
         box[user[0]][user[1]] = 'x'
-        if Win_g(box):
-            Print_box(box, print_key)
-            print("Вы победили !!!")
-            break
+
+     
+        
         
         if 0 == len(unique):
             Print_box(box, print_key)
@@ -83,11 +70,9 @@ while True:
         bot = random.choice(unique)
         unique.remove(bot)
         box[bot[0]][bot[1]] = 'o'
-        if Win_g(box):
-            Print_box(box, print_key)
-            print("Бот выиграл !!!")
-            break
 
+
+        
     else:
         Print_box(box, print_key)
         print("Ничья")
